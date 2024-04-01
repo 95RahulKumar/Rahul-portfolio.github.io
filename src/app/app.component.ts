dab3d46db46fa0404722a5816e9fd9b0f06f3134
Rahul Kumar <mrrahulkumarvns@gmail.com>
2024-04-01 07:49:02 +0530

    new chnges added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
 import { trigger } from '@angular/animations';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { timeInterval } from 'rxjs';
+import LocomotiveScroll from 'locomotive-scroll';
+import Scrollbar from 'smooth-scrollbar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,28 +18,156 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 })
 export class AppComponent implements OnInit {
   title = 'Gsap';
+  scroll!: LocomotiveScroll;
+
   @ViewChild('square') square!: ElementRef;
+  @ViewChild('scrollContent') scrollContent!: ElementRef;
+  showCloseBtn: boolean = false;
   constructor() {
     gsap.registerPlugin(ScrollTrigger);
+    // gsap.registerPlugin(CSSRulePlugin);
   }
   ngOnInit(): void {
-    this.something();
+    // this.something();
+    this.animate();
+    this.applyBg();
+    this.slids();
+  }
+
+  animate() {
+    const scroller = document.querySelector('.scroller') as any;
+    const bodyScrollBar = Scrollbar.init(scroller, {
+      damping: 0.1,
+      // delegateTo: document,
+      renderByPixels: true,
+      delegateTo: document,
+    });
+
+    ScrollTrigger.scrollerProxy('.scroller', {
+      scrollTop(value) {
+        if (arguments.length) {
+          bodyScrollBar.scrollTop = value as number;
+        }
+        return bodyScrollBar.scrollTop;
+      },
+    });
+
+    bodyScrollBar.addListener(ScrollTrigger.update);
+
+    ScrollTrigger.defaults({ scroller: scroller });
+
+    let tl = gsap.timeline();
+    let photos = gsap.utils.toArray('.photo');
+    gsap.set('.photo:not(:first-child)', { y: '100%' });
+    tl.to('.background', {
+      '--path': '15%',
+      duration: 2,
+      ease: 'power2',
+      scrollTrigger: {
+        trigger: '.main',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true,
+        pin: '.main',
+      },
+    }).to('.photo:not(:first-child)', {
+      y: 0,
+      ease: 'power2', // Use 'none' easing for linear animation
+      scrollTrigger: {
+        trigger: '.gallery',
+        start: 'top top',
+        end: 'bottom bottom',
+        toggleActions: 'restart none reverse none',
+        pin: '.right',
+        pinType: 'transform',
+        snap: 1 / (photos.length - 1),
+        scrub: true, // Use true instead of a value for automatic scrubbing optimization
+      },
+    });
+
+    // Start the cards off-screen (adjust the value as needed)
+  }
+  applyBg() {
+    let tl = gsap.timeline();
+    // Section one
+    gsap.to('.gallery', {
+      immediateRender: false,
+      scrollTrigger: {
+        trigger: '.section-one',
+        start: 'top center',
+        end: 'bottom center',
+        toggleActions: 'restart none restart none',
+        onEnter: () => {
+          gsap.to('.gallery', {
+            duration: 1, // Adjust duration as needed
+            backgroundColor: '#FAE1EE',
+            ease: 'power1.inOut',
+          });
+        },
+        onLeaveBack: () => {
+          gsap.to('.gallery', {
+            duration: 1, // Adjust duration as needed
+            ease: 'power1.inOut',
+            backgroundColor: '#FFFFFF',
+          });
+        },
+      },
+    });
+
+    // Section two
+    gsap.to('.gallery', {
+      immediateRender: false,
+      scrollTrigger: {
+        trigger: '.section-two',
+        start: 'top center',
+        end: 'bottom center',
+        toggleActions: 'restart none restart none',
+        onEnter: () => {
+          gsap.to('.gallery', {
+            duration: 1, // Adjust duration as needed
+            ease: 'power1.inOut',
+            backgroundColor: '#E0F0FF',
+          });
+        },
+        onLeaveBack: () => {
+          gsap.to('.gallery', {
+            duration: 1, // Adjust duration as needed
+            ease: 'power1.inOut',
+            backgroundColor: '#FAE1EE',
+          });
+        },
+      },
+    });
+
+    // Section three
+    gsap.to('.gallery', {
+      immediateRender: false,
+      scrollTrigger: {
+        trigger: '.section-three',
+        start: 'top center',
+        end: 'bottom center',
+        toggleActions: 'restart none restart none',
+        onEnter: () => {
+          gsap.to('.gallery', {
+            duration: 1, // Adjust duration as needed
+            ease: 'power1.inOut',
+            backgroundColor: '#FFEDE0',
+          });
+        },
+        onLeaveBack: () => {
+          gsap.to('.gallery', {
+            duration: 1, // Adjust duration as needed
+            ease: 'power1.inOut',
+            backgroundColor: '#E0F0FF',
+          });
+        },
+      },
+    });
   }
   something() {
-    // gsap.to('.square', { x: 677, duration: 3, scrollTrigger: '.square' });
-    // here scrollTrigger will take point when sroll trigger so in this case
-    // when square will comes into viewport then scrollTrigger will trigger
-    // but what if we want trigger scroll when  scrollTrigger point center of the viewport
-    // or some pixel from viewport
-    // so here scrollTrigger:start/end properties will be used
-    // eg
-    // scrollTrigger:{
-    //   trigger:'.square'  // trigger element
-    // }
     gsap.to('.square', {
       x: 700,
       duration: 3,
-
       scrollTrigger: {
         trigger: '.square',
         // now here we will trigger the ele on diff point of viewport
@@ -61,5 +198,51 @@ export class AppComponent implements OnInit {
       },
     });
   }
+  //sliding technoligy section
+  slids() {
+    let sections = gsap.utils.toArray('.slid');
+
+    gsap.to(sections, {
+      xPercent: -80 * (sections.length - 1),
+      ease: 'power2',
+      scrollTrigger: {
+        trigger: '.slids',
+        pin: '.slid',
+        scrub: true,
+        // snap: 1 / (sections.length - 1),
+        start: 'top center',
+        end: 'bottom center',
+      },
+    });
+  }
+  // Animation function for showing the overlay menu
+  showMenu() {
+    this.showCloseBtn = true;
+    gsap.to('.overlay-menu', {
+      duration: 0.5,
+      // opacity: 1,
+      '--clip': '150%',
+      pointerEvents: 'auto',
+      ease: 'power2.inOut',
+    });
+  }
+
+  // Animation function for hiding the overlay menu
+  async hideMenu() {
+    const done = await this.hideAnimationbtn();
+    if (done) this.showCloseBtn = false;
+  }
+
+  hideAnimationbtn(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      gsap.to('.overlay-menu', {
+        duration: 0.5,
+        // opacity: 0,
+        '--clip': '0%',
+        pointerEvents: 'none',
+        ease: 'power2.inOut',
+        onComplete: () => resolve(true),
+      });
+    });
+  }
 }
-9;
